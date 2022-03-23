572614b3c27ec46d8fb8964aeef74a4dcb459fe3
StutiSrivastav <stuti4srivastav@gmail.com>
2022-03-23 12:25:26 +0530

    commit

---
+++ java/capgMaven/Factorial.java
@@ -0,0 +1,15 @@
+package capgMaven;
+
+public class Factorial {
+	
+	public int getFactorial(int n) {
+		int  fact=1;
+		for (int i = 1; i <= n; i++) {
+			fact=fact*i;
+		}
+		return fact;
+	}
+
+	
+
+}
